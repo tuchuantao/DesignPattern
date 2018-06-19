bfb11a7a695d9c851703d30690d4225cb0d85f16
tuchuantao <tuchuantao@xiaomi.com>
2018-06-19 12:07:48 +0800

    结构型设计模式

--- app/src/main/java/com/kevin/designpattern/proxy/MyInvocationHandler.java
+++ app/src/main/java/com/kevin/designpattern/proxy/MyInvocationHandler.java
@@ -14,8 +14,43 @@ public class MyInvocationHandler implements InvocationHandler {
         target = object;
     }
 
+    /**
+     *
+     * @param proxy 是代理类，不是被代理类
+     * @param method
+     * @param args
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         return method.invoke(target, args);
     }
+
+
+/*
+    public final class ProxyImpl extends Proxy implements ProxyInf {
+        private static Method m3;
+        private static Method m1;
+        private static Method m0;
+        private static Method m2;
+
+        public ProxyImpl(InvocationHandler paramInvocationHandler)
+                throws {
+            super(paramInvocationHandler);
+        }
+
+        public final void say()
+                throws {
+            try {
+                this.h.invoke(this, m3, null);
+                return;
+            } catch (RuntimeException localRuntimeException) {
+                throw localRuntimeException;
+            } catch (Throwable localThrowable) {
+            }
+            throw new UndeclaredThrowableException(localThrowable);
+        }
+     ...
+    }*/
 }
