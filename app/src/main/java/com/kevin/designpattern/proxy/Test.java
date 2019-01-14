56ec7a96117fbd5375861ceeb19498e270ccaa9b
tuchuantao <tuchuantao@xiaomi.com>
2019-01-14 20:30:58 +0800

    update adapter uml

--- app/src/main/java/com/kevin/designpattern/proxy/Test.java
+++ app/src/main/java/com/kevin/designpattern/proxy/Test.java
@@ -1,6 +1,8 @@
 package com.kevin.designpattern.proxy;
 
 import java.lang.reflect.Proxy;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Kevin-Tu on 2018/6/14.
@@ -8,9 +10,18 @@ import java.lang.reflect.Proxy;
 public class Test {
 
     public static void main(String[] args) {
-        Student student = new Student();
+        /*Student student = new Student();
         MyInvocationHandler handler = new MyInvocationHandler(student);
         Person person = (Person) Proxy.newProxyInstance(Student.class.getClassLoader(), Student.class.getInterfaces(), handler);
-        person.sayHello();
+        person.sayHello();*/
+
+
+
+        String str = "123";
+
+        Pattern pattern = Pattern.compile("^([0-9]{3}|[A-Za-z]{3})([^ ]*)$");
+
+        Matcher isNum = pattern.matcher(str);
+        System.out.println("result: "+ isNum.matches());
     }
 }
